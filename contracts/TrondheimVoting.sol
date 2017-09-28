57ffbb732bf90d01b22e5cccd21d3773b45e30e2
Kim Borgen <kimatborgen@gmail.com>
2017-09-28 18:59:26 +0200

    add addVoter func

--- contracts/TrondheimVoting.sol
+++ contracts/TrondheimVoting.sol
@@ -5,31 +5,18 @@ contract TrondheimVoting {
 	struct Voter {
 		bool registrered;
 		bool voted;
-		adress delegate;
+		address delegate;
 	}
 	mapping (address => Voter) voters;
-
-	adress public owner;
-
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	address public owner;
 
 	function TrondheimVoting() {
 		owner = msg.sender;
 	}
 
-	function sendCoin(address receiver, uint amount) returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		Transfer(msg.sender, receiver, amount);
-		return true;
-	}
-
-	function getBalanceInEth(address addr) returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
-	}
-
-	function getBalance(address addr) returns(uint) {
-		return balances[addr];
+	function addVoter(address voter) {
+		require(msg.sender == owner);
+		if (msg.sender != owner) { throw; }
+		voters[voter].registrered = true;
 	}
 }
